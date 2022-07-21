d938b4bdde981066b079ed53f0f658c0cc37660b
TS93 <tschram93@yahoo.com>
2022-07-21 16:26:55 -0400

    function for addTask button updating the state

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ function App() {
   //Taking information from input field as a string, also as useState
   const [newTask, setNewTask] = useState('');
 
+  const addTask = (event) => {
+    const newTaskList = [...taskList, newTask] // combining task array string from input;
+    setTaskList(setNewTask); // Taking the new state values and updating them;
+  };
+
   const handleChange = (event) => {
     setNewTask(event.target.value);
-  };
+  }; 
+  {/* created function to handle the input 
+and add it as a string value */}
   
   return (
     <div className="App">
       <h1>Task Manager</h1>
       <div className='addTask'>
         <input onChange={handleChange} /> {/* Whenever typed within input we update the state of newTask */}
-        <button id='taskButton'>Add Task</button>
+        <button onClick={addTask} id='taskButton'>Add Task</button>
       </div>
       <div className='list'></div>
     </div>
