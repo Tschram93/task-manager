ccf8bc412da06abea3f477cfa1651b8fd69827a5
TS93 <tschram93@yahoo.com>
2022-07-21 16:10:46 -0400

    empty array as useState taskList & input field info as a string using useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Want the tasks put into an array, which we are using useState for 
+  const [taskList, setTaskList] = useState([]);
+
+  //Taking information from input field as a string, also as useState
+  const [newTask, setNewTask] = useState('');
+
   return (
     <div className="App">
       <h1>Task Manager</h1>
