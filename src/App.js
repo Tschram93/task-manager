6a370ae83f0cecab0a4f3f7fa4d6692ebbadefbc
TS93 <tschram93@yahoo.com>
2022-07-21 16:41:45 -0400

    using map to output each value in taskList as an h1

--- src/App.js
+++ src/App.js
@@ -2,33 +2,41 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  // Want the tasks put into an array, which we are using useState for 
-  const [taskList, setTaskList] = useState([]);
+	// Want the tasks put into an array, which we are using useState for
+	const [taskList, setTaskList] = useState([]);
 
-  //Taking information from input field as a string, also as useState
-  const [newTask, setNewTask] = useState('');
+	//Taking information from input field as a string, also as useState
+	const [newTask, setNewTask] = useState('');
 
-  const addTask = (event) => {
-    const newTaskList = [...taskList, newTask] // combining task array string from input;
-    setTaskList(setNewTask); // Taking the new state values and updating them;
-  };
+	const addTask = (event) => {
+		const newTaskList = [...taskList, newTask]; // combining task array string from input;
+		setTaskList(newTaskList); // Taking the new state values and updating them;
+    console.log(newTaskList);
+	};
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  }; 
-  {/* created function to handle the input 
-and add it as a string value */}
-  
-  return (
-    <div className="App">
-      <h1>Task Manager</h1>
-      <div className='addTask'>
-        <input onChange={handleChange} /> {/* Whenever typed within input we update the state of newTask */}
-        <button onClick={addTask} id='taskButton'>Add Task</button>
-      </div>
-      <div className='list'></div>
-    </div>
-  );
+	const handleChange = (event) => {
+		setNewTask(event.target.value);
+	};
+	{
+		/* created function to handle the input 
+and add it as a string value */
+	}
+
+	return (
+		<div className='App'>
+			<h1>Task Manager</h1>
+			<div className='addTask'>
+				<input onChange={handleChange} />{' '}
+				{/* Whenever typed within input we update the state of newTask */}
+				<button onClick={addTask} id='taskButton'>
+					Add Task
+				</button>
+			</div>
+			<div className='list'>
+				{taskList.map( task => <h1>{task}</h1>)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
