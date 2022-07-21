daef6957c6d67331557afb5fee6cde7d3de8b3a4
TS93 <tschram93@yahoo.com>
2022-07-21 16:03:16 -0400

    input tag added for tasks with add task button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Task Manager</h1>
+      <div className='addTask'>
+        <input />
+        <button id='taskButton'>Add Task</button>
+      </div>
+      <div className='list'></div>
     </div>
   );
 }
