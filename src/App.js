3d723bf3d68de69163620d87faed9e1403e3439d
TS93 <tschram93@yahoo.com>
2022-07-21 16:16:26 -0400

    created function to handle the input and add it as a string value

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
   //Taking information from input field as a string, also as useState
   const [newTask, setNewTask] = useState('');
 
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  
   return (
     <div className="App">
       <h1>Task Manager</h1>
       <div className='addTask'>
-        <input />
+        <input onChange={handleChange} /> {/* Whenever typed within input we update the state of newTask */}
         <button id='taskButton'>Add Task</button>
       </div>
       <div className='list'></div>
